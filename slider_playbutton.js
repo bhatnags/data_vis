bf97a0af4473247e31acddb1c96fc68e80659e26
bhatnags <35749734+bhatnags@users.noreply.github.com>
2020-11-24 14:30:47 +0000

    Create slider_playbutton.js

---
+++ slider_playbutton.js
@@ -0,0 +1,39 @@
+// getData(xValue, yValue, sliderValue);
+
+// http://thematicmapping.org/playground/d3/d3.slider/
+
+
+// https://bl.ocks.org/johnwalley/e1d256b81e51da68f7feb632a53c3518
+// Time
+// var data = [0, 0.005, 0.01, 0.015, 0.02, 0.025];
+
+var time_seconds = d3.range(0, 20).map(function(d) {
+  // return new Date(1995 + d, 10, 3);
+  return d;
+});
+
+ var sliderTime = d3
+   .sliderBottom()
+   .min(d3.min(time_seconds))
+   .max(d3.max(time_seconds))
+   .step(1000 * 60 * 60 * 24 * 365)
+   .width(300)
+   //.tickFormat(d3.timeFormat('%Y'))
+   .tickValues(time_seconds)
+   //.default(new Date(1998, 10, 3))
+   .default(5)
+   .on('onchange', val => {
+     d3.select('p#value-time').text(val);
+   });
+
+ var gTime = d3
+   .select('div#slider-time')
+   .append('svg')
+   .attr('width', 1500)
+   //.attr('height', 100)
+   .append('g')
+   .attr('transform', 'translate(30,30)');
+
+ gTime.call(sliderTime);
+
+ d3.select('p#value-time').text(sliderTime.value());
