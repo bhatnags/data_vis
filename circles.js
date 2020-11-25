9b61e1b48e2d9e962ae0c1a078d395c7e3592498
bhatnags <35749734+bhatnags@users.noreply.github.com>
2020-11-25 16:56:10 +0000

    Update circles.js

--- circles.js
+++ circles.js
@@ -1,9 +1,13 @@
 var width = 1500;
 var height = 1000;
 var start = 200;
-var circle_radius = 30;
+
 var gap = 200;
 var controllables = ["c1", "c2", "c3", "c4", "c5", "c6"];
+
+var circle_radius = 30;
+var rect_width = 80;
+var rect_height = 20;
 
 var svg = d3.select("#svgcontainer")
    .append("svg")
@@ -53,4 +57,47 @@ group.append("text")
    .attr("font-family", "sans-serif")
    .text(function(d) {
       return d;
-   });
+   })
+   
+group.append("rect")
+   .attr("x", function(d, i) {
+      return start + i*gap - circle_radius;
+   })      
+   .attr("y", function(d, i) {
+      if (i<3){
+         return start + i*gap+3* circle_radius;
+      } else {
+         return start + (5-i)*gap + 3*circle_radius;
+      }
+   })
+   .attr("width", rect_width)
+   .attr("height", rect_height)
+   .attr("fill", "red")
+   //.attr("transform", "rotate(90)")
+
+group.append("rect")
+   .attr("x", function(d, i) {
+      if (i<2 || i==3 || i==5){
+         return start + i*gap - circle_radius;
+      } else {
+         return start - 5*circle_radius;
+      }
+   })      
+   .attr("y", function(d, i) {
+      if (i<2){
+         return start + i*gap-4* circle_radius;
+      } else if (i==3 || i==5){
+         return start + (5-i)*gap - 4*circle_radius;
+      } else if (i==2){ 
+         return start + (4-1*i)*gap - 12*circle_radius;
+      } else if (i==4){ // 2 and 4
+      return start + (4-1*i)*gap - 2*circle_radius;
+   }
+})
+   .attr("width", rect_width)
+   .attr("height", rect_height)
+   .attr("fill", "red")
+   //.attr("transform", "rotate(90)")
+
+
+   ;
