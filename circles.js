7ee32ffbf0b5b1e1fe5f2ff712d9a9ecc162d690
bhatnags <35749734+bhatnags@users.noreply.github.com>
2020-11-25 13:55:51 +0000

    Update circles.js

--- circles.js
+++ circles.js
@@ -1,9 +1,9 @@
 var width = 1500;
 var height = 1000;
-var start = 50;
+var start = 200;
 var circle_radius = 30;
-var gap = 100;
-var junc_names = ["j1", "j2", "j3", "j4", "j5", "j6"];
+var gap = 200;
+var controllables = ["c1", "c2", "c3", "c4", "c5", "c6"];
 
 var svg = d3.select("#svgcontainer")
    .append("svg")
@@ -12,7 +12,7 @@ var svg = d3.select("#svgcontainer")
 
 
 var group = svg.selectAll("g")
-   .data(junc_names)
+   .data(controllables)
    .enter()
    .append("g")
    //.attr("transform", "translate(60, 60) rotate(30)");
@@ -23,12 +23,16 @@ var group = svg.selectAll("g")
 
 group.append("circle")
    .attr("cx", function(d, i) {
-            return start + i*gap;
-         })
+         return start + i*gap;
+     })
          
    .attr("cy", function(d, i) {
-            return start + i*gap;
-         })
+      if (i<3){
+         return start + i*gap;
+      } else {
+         return start + (5-i)*gap ;
+      }
+   })
   
    .attr("r", circle_radius)
    .attr("fill", "red")
@@ -38,8 +42,12 @@ group.append("text")
       return start + i * gap;
    })
    .attr("y", function(d, i) {
-            return start + i*gap;
-         })
+      if (i<3){
+         return start + i*gap;
+      } else {
+         return start + (5-i)*gap ;
+      }
+   })
    .attr("stroke", "teal")
    .attr("font-size", "10px")
    .attr("font-family", "sans-serif")
