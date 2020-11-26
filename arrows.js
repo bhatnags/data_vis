6c3e2bf6dada9469b0bfa23d2ef45b434ef021f0
bhatnags <35749734+bhatnags@users.noreply.github.com>
2020-11-26 13:35:04 +0000

    Update arrows.js

--- arrows.js
+++ arrows.js
@@ -1,57 +1,435 @@
-this is arrows.js
-
-
-/*var w = 100;
-var h = 100;
-var arrow_group = d3.select("#chart")
-   .append("svg")
-   .attr("width", w)
-   .attr("height", h),//;
-*/
-
-
-var data = [0 19, 1 9, 2 0, 3 48, 4 79, 5 90, 6 1];
-
-
 var arrow_group = svg.append("g")
-   //.attr("transform", "translate(60, 60) rotate(30)");
-   .attr("transform", "translate(60, 60) rotate(-40)")/*,
-   scaleX = d3.scale.linear()
-           .domain([-30,30])
-           .range([0,600]),
-   scaleY = d3.scale.linear()
-           .domain([0,50])
-           .range([500,0]),
-   poly = [{"x":5, "y":30},
-           {"x":15,"y":10},
-           {"x":25,"y":30}]*/;
+   .attr("transform", "translate(600, 0) rotate(0)"),
+   scaleX = d3.scaleLinear()
+      .domain([0,300])
+      .range([0,600]),
+   scaleY = d3.scaleLinear()
+      .domain([0,500])
+      .range([500,0]),
+   poly = [{"x":15, "y":30}, {"x":15,"y":10}, {"x":25,"y":30}],
+   rect_coord1 = [{"x":140, "y":-35}],
+   rect_coord2= [{"x":30, "y":-25}]
+   ;
+
+   /*************************************************************/
+   /************************ FIRST JUNC *******************************/
+   /*************************************************************/
 
 
-
-/*arrow_group.selectAll("polygon")
+   /************************ DOWN ARROW *******************************/
+   var triangle = arrow_group.selectAll("g").append("polygon")
    .data([poly])
-   .enter()
-   .append("polygon")
-   .attr("points", function(d){
-      return d.map(function(d) {
-         return [scaleX(d.x), scaleY(d.y)].join(",");
+   .enter().append("polygon")
+   .attr("points",function(d) { 
+      return d.map(function(d) { 
+         return [scaleX(d.x),scaleY(d.y)].join(","); 
       }).join(" ");
    })
-   .attr("stroke", "blue")
-   .attr("fill", "black")
-   .attr("stroke-width", 2)
-*/
-var triangle = arrow_group.append("polygon")
-   .attr("points", "05,30 15,10 25,30")
-   .attr("fill", "black")
-   .attr("stroke", "blue")
-   .attr("stroke-width","2")
+   .attr("stroke","black")
+   .attr("stroke-width",2)
+   .attr("transform", "translate(-680, 520) rotate(-135)");
+  
+   /************************ UP ARROW *******************************/
+   var triangle = arrow_group.selectAll("g").append("polygon")
+   .data([poly])
+   .enter().append("polygon")
+   .attr("points",function(d) { 
+      return d.map(function(d) { 
+         return [scaleX(d.x),scaleY(d.y)].join(","); 
+      }).join(" ");
+   })
+   .attr("stroke","black")
+   .attr("stroke-width",2)
+   .attr("transform", "translate(-90, -250) rotate(45)");  
+   //.attr("transform", function(d){
+   //    return 'translate(' + -90 + ',' + -250 + ') rotate(45)")'  
+   //});
+   
+   /************************ DOWN ARROW *******************************/
+   var triangle = arrow_group.selectAll("g").append("polygon")
+   .data([poly])
+   .enter().append("polygon")
+   .attr("points",function(d) { 
+      return d.map(function(d) { 
+         return [scaleX(d.x),scaleY(d.y)].join(","); 
+      }).join(" ");
+   })
+   .attr("stroke","black")
+   .attr("stroke-width",2)
+   .attr("transform", "translate(-680, 640) rotate(-135)");
+ 
+   /************************ UP ARROW *******************************/
+   var triangle = arrow_group.selectAll("g").append("polygon")
+   .data([poly])
+   .enter().append("polygon")
+   .attr("points",function(d) { 
+      return d.map(function(d) { 
+         return [scaleX(d.x),scaleY(d.y)].join(","); 
+      }).join(" ");
+   })
+   .attr("stroke","black")
+   .attr("stroke-width",2)
+   .attr("transform", "translate(-90, -130) rotate(45)");  
+   
+
+   /************************ ANGLED ARROW *******************************/
+   var triangle = arrow_group.selectAll("g").append("polygon")
+   .data([poly])
+   .enter().append("polygon")
+   .attr("points",function(d) { 
+      return d.map(function(d) { 
+         return [scaleX(d.x),scaleY(d.y)].join(","); 
+      }).join(" ");
+   })
+   .attr("stroke","black")
+   .attr("stroke-width",2)
+   .attr("transform", "translate(-540, -370) rotate(0)");  
 
 
-var rect = arrow_group.append("rect")
+   var triangle = arrow_group.selectAll("g").append("polygon")
+   .data([poly])
+   .enter().append("polygon")
+   .attr("points",function(d) { 
+      return d.map(function(d) { 
+         return [scaleX(d.x),scaleY(d.y)].join(","); 
+      }).join(" ");
+   })
+   .attr("stroke","black")
+   .attr("stroke-width",2)
+   .attr("transform", "translate(-440, 660) rotate(180)");  
+
+
+   var triangle = arrow_group.selectAll("g").append("polygon")
+   .data([poly])
+   .enter().append("polygon")
+   .attr("points",function(d) { 
+      return d.map(function(d) { 
+         return [scaleX(d.x),scaleY(d.y)].join(","); 
+      }).join(" ");
+   })
+   .attr("stroke","black")
+   .attr("stroke-width",2)
+   .attr("transform", "translate(-980, 330) rotate(-90)");  
+
+
+   var triangle = arrow_group.selectAll("g").append("polygon")
+   .data([poly])
+   .enter().append("polygon")
+   .attr("points",function(d) { 
+      return d.map(function(d) { 
+         return [scaleX(d.x),scaleY(d.y)].join(","); 
+      }).join(" ");
+   })
+   .attr("stroke","black")
+   .attr("stroke-width",2)
+   .attr("transform", "translate(-20, 340) rotate(110)");  
+
+
+
+
+
+
+
+   /*************************************************************/
+   /************************ SECOND JUNC *******************************/
+   /*************************************************************/
+   
+   /************************ DOWN ARROW *******************************/
+   var triangle = arrow_group.selectAll("g").append("polygon")
+   .data([poly])
+   .enter().append("polygon")
+   .attr("points",function(d) { 
+      return d.map(function(d) { 
+         return [scaleX(d.x),scaleY(d.y)].join(","); 
+      }).join(" ");
+   })
+   .attr("stroke","black")
+   .attr("stroke-width",2)
+   .attr("transform", "translate(-480, 720) rotate(-135)");
+   
+   /************************ UP ARROW *******************************/
+   var triangle = arrow_group.selectAll("g").append("polygon")
+   .data([poly])
+   .enter().append("polygon")
+   .attr("points",function(d) { 
+      return d.map(function(d) { 
+         return [scaleX(d.x),scaleY(d.y)].join(","); 
+      }).join(" ");
+   })
+   .attr("stroke","black")
+   .attr("stroke-width",2)
+   .attr("transform", "translate(100, -50) rotate(45)");
+
+   /************************ DOWN ARROW *******************************/
+   var triangle = arrow_group.selectAll("g").append("polygon")
+   .data([poly])
+   .enter().append("polygon")
+   .attr("points",function(d) { 
+      return d.map(function(d) { 
+         return [scaleX(d.x),scaleY(d.y)].join(","); 
+      }).join(" ");
+   })
+   .attr("stroke","black")
+   .attr("stroke-width",2)
+   .attr("transform", "translate(-480, 840) rotate(-135)");
+   
+
+   /************************ UP ARROW *******************************/
+   var triangle = arrow_group.selectAll("g").append("polygon")
+   .data([poly])
+   .enter().append("polygon")
+   .attr("points",function(d) { 
+      return d.map(function(d) { 
+         return [scaleX(d.x),scaleY(d.y)].join(","); 
+      }).join(" ");
+   })
+   .attr("stroke","black")
+   .attr("stroke-width",2)
+   .attr("transform", "translate(100, 70) rotate(45)");
+
+
+
+   /*************************************************************/
+   /************************ THIRD JUNC *******************************/
+   /*************************************************************/
+
+
+   /************************ DOWN ARROW *******************************/
+   var triangle = arrow_group.selectAll("g").append("polygon")
+   .data([poly])
+   .enter().append("polygon")
+   .attr("points",function(d) { 
+      return d.map(function(d) { 
+         return [scaleX(d.x),scaleY(d.y)].join(","); 
+      }).join(" ");
+   })
+   .attr("stroke","black")
+   .attr("stroke-width",2)
+   .attr("transform", "translate(-280, 1040) rotate(-135)");
+
+   /************************ UP ARROW *******************************/
+   var triangle = arrow_group.selectAll("g").append("polygon")
+   .data([poly])
+   .enter().append("polygon")
+   .attr("points",function(d) { 
+      return d.map(function(d) { 
+         return [scaleX(d.x),scaleY(d.y)].join(","); 
+      }).join(" ");
+   })
+   .attr("stroke","black")
+   .attr("stroke-width",2)
+   .attr("transform", "translate(300, 270) rotate(45)");
+
+
+    
+
+
+   /*************************************************************/
+   /************************ FOURTH JUNC *******************************/
+   /*************************************************************/
+   
+
+
+
+    /************************ DOWN ARROW *******************************/
+    var triangle = arrow_group.selectAll("g").append("polygon")
+   .data([poly])
+   .enter().append("polygon")
+   .attr("points",function(d) { 
+      return d.map(function(d) { 
+         return [scaleX(d.x),scaleY(d.y)].join(","); 
+      }).join(" ");
+   })
+   .attr("stroke","black")
+   .attr("stroke-width",2)
+   .attr("transform", "translate(-80, 920) rotate(-135)");
+
+
+   /************************ UP ARROW *******************************/
+   var triangle = arrow_group.selectAll("g").append("polygon")
+   .data([poly])
+   .enter().append("polygon")
+   .attr("points",function(d) { 
+      return d.map(function(d) { 
+         return [scaleX(d.x),scaleY(d.y)].join(","); 
+      }).join(" ");
+   })
+   .attr("stroke","black")
+   .attr("stroke-width",2)
+   .attr("transform", "translate(500, 150) rotate(45)");
+
+    /************************ DOWN ARROW *******************************/
+    var triangle = arrow_group.selectAll("g").append("polygon")
+   .data([poly])
+   .enter().append("polygon")
+   .attr("points",function(d) { 
+      return d.map(function(d) { 
+         return [scaleX(d.x),scaleY(d.y)].join(","); 
+      }).join(" ");
+   })
+   .attr("stroke","black")
+   .attr("stroke-width",2)
+   .attr("transform", "translate(-80, 1040) rotate(-135)");
+
+   /************************ UP ARROW *******************************/
+   var triangle = arrow_group.selectAll("g").append("polygon")
+   .data([poly])
+   .enter().append("polygon")
+   .attr("points",function(d) { 
+      return d.map(function(d) { 
+         return [scaleX(d.x),scaleY(d.y)].join(","); 
+      }).join(" ");
+   })
+   .attr("stroke","black")
+   .attr("stroke-width",2)
+   .attr("transform", "translate(500, 270) rotate(45)");
+
+
+
+   /*************************************************************/
+   /************************ FIFTH JUNC *******************************/
+   /*************************************************************/
+
+
+   /************************ DOWN ARROW *******************************/
+   var triangle = arrow_group.selectAll("g").append("polygon")
+   .data([poly])
+   .enter().append("polygon")
+   .attr("points",function(d) { 
+      return d.map(function(d) { 
+         return [scaleX(d.x),scaleY(d.y)].join(","); 
+      }).join(" ");
+   })
+   .attr("stroke","black")
+   .attr("stroke-width",2)
+   .attr("transform", "translate(120, 840) rotate(-135)");
+
+   /************************ UP ARROW *******************************/
+   var triangle = arrow_group.selectAll("g").append("polygon")
+   .data([poly])
+   .enter().append("polygon")
+   .attr("points",function(d) { 
+      return d.map(function(d) { 
+         return [scaleX(d.x),scaleY(d.y)].join(","); 
+      }).join(" ");
+   })
+   .attr("stroke","black")
+   .attr("stroke-width",2)
+   .attr("transform", "translate(700, 70) rotate(45)");
+
+
+    
+
+  /*************************************************************/
+   /************************ SIXTH JUNC *******************************/
+   /*************************************************************/
+   
+
+   /************************ DOWN ARROW *******************************/
+   var triangle = arrow_group.selectAll("g").append("polygon")
+   .data([poly])
+   .enter().append("polygon")
+   .attr("points",function(d) { 
+      return d.map(function(d) { 
+         return [scaleX(d.x),scaleY(d.y)].join(","); 
+      }).join(" ");
+   })
+   .attr("stroke","black")
+   .attr("stroke-width",2)
+   .attr("transform", "translate(320, 520) rotate(-135)");
+
+
+   
+   /************************ UP ARROW *******************************/
+   var triangle = arrow_group.selectAll("g").append("polygon")
+   .data([poly])
+   .enter().append("polygon")
+   .attr("points",function(d) { 
+      return d.map(function(d) { 
+         return [scaleX(d.x),scaleY(d.y)].join(","); 
+      }).join(" ");
+   })
+   .attr("stroke","black")
+   .attr("stroke-width",2)
+   .attr("transform", "translate(900, -250) rotate(45)");
+
+
+   /************************ DOWN ARROW *******************************/
+   var triangle = arrow_group.selectAll("g").append("polygon")
+   .data([poly])
+   .enter().append("polygon")
+   .attr("points",function(d) { 
+      return d.map(function(d) { 
+         return [scaleX(d.x),scaleY(d.y)].join(","); 
+      }).join(" ");
+   })
+   .attr("stroke","black")
+   .attr("stroke-width",2)
+   .attr("transform", "translate(320, 640) rotate(-135)");
+
+   /************************ UP ARROW *******************************/
+   var triangle = arrow_group.selectAll("g").append("polygon")
+   .data([poly])
+   .enter().append("polygon")
+   .attr("points",function(d) { 
+      return d.map(function(d) { 
+         return [scaleX(d.x),scaleY(d.y)].join(","); 
+      }).join(" ");
+   })
+   .attr("stroke","black")
+   .attr("stroke-width",2)
+   .attr("transform", "translate(900, -130) rotate(45)");
+
+
+
+
+var rect = arrow_group.selectAll("g").append("rect")
+   .data([rect_coord1, rect_coord2])
+   .enter().append("rect")
+   .attr("x",function(d) { 
+      return scaleX(d.x); 
+   })
+   .attr("y",function(d) { 
+      return scaleY(d.y); 
+   })
+   .attr("width", 60)
+   .attr("height", 20)
+   .attr("fill", "green")
+   .attr("transform", "translate(0, 200) rotate(90)")
+
+/*var rect = arrow_group.append("rect")
    .attr("x", 30)
    .attr("y", -25)
    .attr("width", 60)
    .attr("height", 20)
    .attr("fill", "green")
    .attr("transform", "rotate(90)")
+*/
+
+
+
+
+
+
+/*var vis = d3.select("body").append("svg")
+   .attr("width", 1000)
+   .attr("height", 667),
+
+scaleX = d3.scaleLinear()
+  .domain([-30,30])
+  .range([0,600]),
+
+scaleY = d3.scaleLinear()
+  .domain([0,50])
+  .range([500,0]),
+
+
+vis.selectAll("polygon")
+.data([poly])
+.enter().append("polygon")
+.attr("points",function(d) { 
+  return d.map(function(d) { return [scaleX(d.x),scaleY(d.y)].join(","); }).join(" ");})
+.attr("stroke","black")
+.attr("stroke-width",2);
+
+*/
